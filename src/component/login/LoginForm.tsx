d5d0521c9f8c90b8b663463d6395b6e894dd5969
konopskyyy <konopskyyy@gmail.com>
2025-10-05 13:42:29 +0200

    add: Login and RemindMe forms, linter

---
+++ src/component/login/LoginForm.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+export default function LoginForm() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function sendForm(e) {
+    e.preventDefault();
+    const response = await fetch(
+      "https://questions.tojest.dev/api/login_check",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: name,
+          password: password,
+        }),
+      },
+    );
+    const data = await response.json();
+    alert(JSON.stringify(data));
+  }
+
+  return (
+    <form onSubmit={sendForm}>
+      <label htmlFor="name">name </label>
+      <input onChange={(e) => setName(e.target.value)} value={name} id="name" />
+      <br />
+      <label htmlFor="password">password </label>
+      <input
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        id="password"
+        type="password"
+      />
+      <br />
+      <button type="submit">Zaloguj</button>
+      <h5>
+        nie pamiętasz hasła? <a href="remind-me">przypomij</a>
+      </h5>
+    </form>
+  );
+}
---
+++ src/component/remind-me/RemindMeForm.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function RemindMeForm() {
+  const [name, setName] = useState("");
+
+  async function sendForm(e) {
+    e.preventDefault();
+    const response = await fetch(
+      "https://questions.tojest.dev/api/user/remind-me",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: name,
+        }),
+      },
+    );
+    const data = await response.json();
+    alert(JSON.stringify(data));
+  }
+
+  return (
+    <form onSubmit={sendForm}>
+      <label htmlFor="name">name </label>
+      <input onChange={(e) => setName(e.target.value)} value={name} id="name" />
+      <br />
+      <button type="submit">Przypomij hasło</button>
+    </form>
+  );
+}
