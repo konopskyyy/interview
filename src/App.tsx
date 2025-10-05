f4af622aea953596d0639785fd8f37551d7d3b98
konopskyyy <konopskyyy@gmail.com>
2025-10-05 13:42:29 +0200

    add: Login and RemindMe forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css'
 import RegisterForm from "./component/register/RegisterForm.tsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginForm from "./component/login/LoginForm.tsx";
+import RemindMeForm from "./component/remind-me/RemindMeForm.tsx";
 
 
 function App() {
@@ -10,7 +12,9 @@ function App() {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<RegisterForm />} />
-                <Route path="/test" element={<RegisterForm />} />
+                <Route path="/login" element={<LoginForm />} />
+                <Route path="/register" element={<RegisterForm />} />
+                <Route path="/remind-me" element={<RemindMeForm />} />
             </Routes>
         </BrowserRouter>
     </>
