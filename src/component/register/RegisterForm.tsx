91e71bb871d390a59daa9c366bed76245fbb370c
konopskyyy <konopskyyy@gmail.com>
2025-10-06 19:09:30 +0200

    run prettier

--- src/component/register/RegisterForm.tsx
+++ src/component/register/RegisterForm.tsx
@@ -1,52 +1,52 @@
-import {useState} from "react";
+import { useState } from "react";
 
 export default function RegisterForm() {
-    const [name, setName] = useState<string>('')
-    const [password, setPassword] = useState<string>('')
-    const [retypedpassword, setretypedpassword] = useState<string>('')
+  const [name, setName] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [retypedpassword, setretypedpassword] = useState<string>("");
 
-    async function sendForm(e) {
-        e.preventDefault();
-        if (password != retypedpassword) {
-            alert('hasla nie zgadzaja sie');
-            return;
-        }
-        const response = await fetch('https://questions.tojest.dev/api/user', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                email: name,
-                password: password
-            })
-        });
-        const data = await response.json();
-        alert(JSON.stringify(data));
+  async function sendForm(e) {
+    e.preventDefault();
+    if (password != retypedpassword) {
+      alert("hasla nie zgadzaja sie");
+      return;
     }
+    const response = await fetch("https://questions.tojest.dev/api/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: name,
+        password: password,
+      }),
+    });
+    const data = await response.json();
+    alert(JSON.stringify(data));
+  }
 
-    return (
-        <form onSubmit={sendForm}>
-            <label for="name">name </label>
-            <input value={name}
-                   id="name"
-                   onChange={(e) => setName(e.target.value)}
-            />
-            <br/>
-            <label htmlFor="password">password </label>
-            <input id="password"
-                   type="password"
-                   value={password}
-                   onChange={(e) => setPassword(e.target.value)}
-            />
-            <br/>
-            <label htmlFor="retype-password">retyped password </label>
-            <input id="retype-password"
-                   value={retypedpassword}
-                   type="password"
-                   onChange={(e) => setretypedpassword(e.target.value)}/>
-            <br/>
-            <button type="submit">Rejestruj</button>
-        </form>
-    )
+  return (
+    <form onSubmit={sendForm}>
+      <label for="name">name </label>
+      <input value={name} id="name" onChange={(e) => setName(e.target.value)} />
+      <br />
+      <label htmlFor="password">password </label>
+      <input
+        id="password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <br />
+      <label htmlFor="retype-password">retyped password </label>
+      <input
+        id="retype-password"
+        value={retypedpassword}
+        type="password"
+        onChange={(e) => setretypedpassword(e.target.value)}
+      />
+      <br />
+      <button type="submit">Rejestruj</button>
+    </form>
+  );
 }
--- src/component/remind-me/RemindMeForm.tsx
+++ src/component/remind-me/RemindMeForm.tsx
@@ -1,29 +1,32 @@
 import { useState } from "react";
 
 export default function RemindMeForm() {
-    const [name, setName] = useState('');
+  const [name, setName] = useState("");
 
-    async function sendForm(e) {
-        e.preventDefault();
-        const response = await fetch('https://questions.tojest.dev/api/user/remind-me', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                email: name,
-            })
-        });
-        const data = await response.json();
-        alert(JSON.stringify(data));
-    }
-
-    return (
-        <form onSubmit={sendForm}>
-            <label htmlFor="name">name </label>
-            <input onChange={e => setName(e.target.value)} value={name} id="name"/>
-            <br/>
-            <button type="submit">Przypomij hasło</button>
-        </form>
+  async function sendForm(e) {
+    e.preventDefault();
+    const response = await fetch(
+      "https://questions.tojest.dev/api/user/remind-me",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: name,
+        }),
+      },
     );
+    const data = await response.json();
+    alert(JSON.stringify(data));
+  }
+
+  return (
+    <form onSubmit={sendForm}>
+      <label htmlFor="name">name </label>
+      <input onChange={(e) => setName(e.target.value)} value={name} id="name" />
+      <br />
+      <button type="submit">Przypomij hasło</button>
+    </form>
+  );
 }
