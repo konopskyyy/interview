877fd0678c5775fd46254669c173ccb7245a387e
konopskyyy <konopskyyy@gmail.com>
2025-11-08 17:07:05 +0100

    add: account page

---
+++ src/page/account/AccountPage.tsx
@@ -0,0 +1,21 @@
+import {useContext, useEffect} from "react";
+import {UserContext} from "../../context/UserContext.tsx";
+import {useNavigate} from "react-router";
+
+export default function AccountPage()
+{
+    const { user } = useContext(UserContext);
+    const { isLogged } = useContext(UserContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!isLogged()) {
+            navigate("/");
+        }
+    }, []);
+
+    return (
+        <h2>Nazwa użytkownika: {user.username}</h2>
+
+    )
+}
--- src/section/Page.tsx
+++ src/section/Page.tsx
@@ -4,6 +4,7 @@ import RegisterForm from "../component/register/RegisterForm.tsx";
 import LoginForm from "../component/login/LoginForm.tsx";
 import RemindMeForm from "../component/remind-me/RemindMeForm.tsx";
 import NotFound from "./NotFound.tsx";
+import AccountPage from "../page/account/AccountPage.tsx";
 
 export default function Page()
 {
@@ -14,6 +15,7 @@ export default function Page()
                 <Route path="/login" element={<LoginForm />} />
                 <Route path="/register" element={<RegisterForm />} />
                 <Route path="/remind-me" element={<RemindMeForm />} />
+                <Route path="/account" element={<AccountPage />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </div>
