a6fd85a26f1a3fb2a30477a4775dbecfe6529e4a
konopskyyy <konopskyyy@gmail.com>
2025-11-16 18:16:28 +0100

    change: client http as service

---
+++ src/service/QuestionApiClient.ts
@@ -0,0 +1,11 @@
+import { post } from "./GenericApiClient.ts";
+
+const BASE_URL = "https://questions.tojest.dev/api/";
+
+export function userLogin(user: { username: string; password: string }) {
+  return post(BASE_URL + "login_check", user, "Błąd logowania");
+}
+
+export function userRegister(user: { email: string; password: string }) {
+  return post(BASE_URL + "user", user, "Błąd rejestracji");
+}
