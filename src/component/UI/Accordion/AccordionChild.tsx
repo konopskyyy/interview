f7dbd3c5dedab1fdbc36b44bf23a9dacea4f9717
konopskyyy <konopskyyy@gmail.com>
2026-01-01 23:33:54 +0100

    add: interview page poc

--- src/component/UI/Accordion/AccordionChild.tsx
+++ src/component/UI/Accordion/AccordionChild.tsx
@@ -1,14 +1,23 @@
 import type { ReactNode } from "react";
+import { useNavigate } from "react-router";
 
 interface AccordionChildProps {
+  id: string;
   title: string;
   children: ReactNode;
 }
 
 export default function AccordionChild({
+  id,
   title,
   children,
 }: AccordionChildProps) {
+  const navigate = useNavigate();
+
+  function handleMoveToInterview(id: string) {
+    navigate("/interview/" + id);
+  }
+
   return (
     <div className="space-y-2 pl-4">
       <details
@@ -17,7 +26,6 @@ export default function AccordionChild({
       >
         <summary className="flex cursor-pointer items-center justify-between gap-4 rounded-lg border border-gray-200 bg-gray-50 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100">
           <span>{title}</span>
-
           <svg
             className="size-4 shrink-0 transition-transform duration-300 group-open/members:-rotate-180"
             xmlns="http://www.w3.org/2000/svg"
@@ -32,6 +40,12 @@ export default function AccordionChild({
               d="M19 9l-7 7-7-7"
             ></path>
           </svg>
+          <button
+            onClick={() => handleMoveToInterview(id)}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Przejdź
+          </button>
         </summary>
 
         <div className="p-4">
