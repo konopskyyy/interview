fbbf26ece0f2f6bd8005b6ee9b8159ceb3a866cf
konopskyyy <konopskyyy@gmail.com>
2026-01-10 11:37:57 +0100

    fix: header color

--- src/component/Header/AnonymousUserHeader.tsx
+++ src/component/Header/AnonymousUserHeader.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router";
 
 export default function AnonymousUserHeader() {
   return (
-    <nav className="bg-gray-800 w-full fixed top-0 left-0 z-50">
+    <nav className="bg-gray-900 w-full fixed top-0 left-0 z-50">
       <div className="container mx-auto flex justify-between items-center h-14 px-6">
         <div className="text-white font-bold text-xl">Interviewer</div>
         <ul className="flex space-x-10 text-gray-300 text-lg">
